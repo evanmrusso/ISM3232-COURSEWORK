28ae22510b3da00b4a80c5fdfb971ae64b816d2a
Evan Russo <evanrusso@usf.edu>
2026-08-24 09:59:02 -0400

    Add hello_ism3232.py with course details

---
+++ module01_setup/hello_ism3232.py
@@ -0,0 +1,12 @@
+# hello_ism3232.py
+# Author: Evan Russo 
+
+print("Hello, ISM3232!")
+
+course_name = 'Business Application Development'
+credit_hours = 3
+weekly_hours = credit_hours * 2
+
+print(f'Course: {course_name}')
+print(f'Expected weekly hours: {weekly_hours}')
+print('Environment verified. Week 1 complete.')
